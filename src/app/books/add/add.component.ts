140997095b5b6d9942dbcecbc6ffd50e5667cd08
Arti-Biswa <aratibiswa481@gmail.com>
2024-12-13 18:54:38 +0600

    LMS-06:Integrated add API for books in frontend

---
+++ src/app/books/add/add.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { BookService } from '../../book.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add',
+  standalone: true,
+  imports: [CommonModule,HttpClientModule,FormsModule],
+  templateUrl: './add.component.html',
+  styleUrl: './add.component.css'
+})
+export class AddComponent {
+  name: string = 'John Doe';  
+  dropdownVisible: boolean = false;
+  book = {
+    isbn: '',
+    title: '',
+    author: '',
+    genre: '',
+    price: 0,
+    availableCopies: 0,
+    publisher: '',
+    publishedYear: 0
+  };
+ 
+ constructor(private bookService: BookService, private router: Router) {}
+
+ saveBook() {
+   this.bookService.saveBook(this.book).subscribe({
+     next: (response) => {
+       alert('Book saved successfully!');
+       console.log(response);
+       this.router.navigate(['books/view']); 
+     },
+     error: (error) => {
+       alert('Failed to save the book.');
+       console.error(error);
+     }
+   });
+ }
+ 
+ toggleProfileDropdown(): void {
+  this.dropdownVisible = !this.dropdownVisible;
+}
+booksDropdownVisible = false; 
+
+toggleBooksDropdown(event: MouseEvent): void {
+  event.preventDefault(); 
+  this.booksDropdownVisible = !this.booksDropdownVisible;
+}
+
+cancelForm(): void {
+  this.router.navigate(['/books/view']);  
+}
+}
